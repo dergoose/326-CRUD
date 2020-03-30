ee385f2e42860c90dfba5ce8773430220ea83701
Veronica <cpgusev@gmail.com>
2020-03-30 16:15:31 -0400

    added heroku

--- pcrud-async-db.js
+++ pcrud-async-db.js
@@ -63,6 +63,35 @@ let server = http.createServer();
 server.on('request', async (request, response) => {
     response.writeHead(200, headerText);
     let options = url.parse(request.url, true).query;
+    
+    if (request.url.endsWith("/index.html")) {
+        fs.readFile('static/pcrud-interactive.html', null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write('Whoops! File not found!');
+            } else {
+                response.writeHead(200, {
+                    "Content-Type": "text/html"});
+                response.write(data);
+            }
+            response.end();
+        });
+        return;
+    } else if (request.url.endsWith("/pcrud-xhr.js")) {
+        fs.readFile('static/pcrud-xhr.js', null, function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write('Whoops! File not found!');
+            } else {
+                response.writeHead(200, {
+                    "Content-Type": "text/javascript"});
+                response.write(data);
+            }
+            response.end();
+        });
+        return;
+    }
+ // Heroku mod ends 
     if (request.url.startsWith("/create")) {
         await createCounter(options.name, response);
         return;
